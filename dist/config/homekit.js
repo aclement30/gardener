69932309f8c4cc4175fec855415283f68923f41f
Alexandre Clément <web@alexandreclement.com>
2018-01-10 23:08:28 -0500

    Fix production homekit config

--- dist/config/homekit.js
+++ dist/config/homekit.js
@@ -2,7 +2,11 @@
 exports.__esModule = true;
 exports["default"] = {
     // DEV
-    name: 'GTest',
-    username: 'CC:22:3D:E3:CE:F7',
-    pincode: '031-45-555'
+    // name: 'GTest',
+    // username: 'CC:22:3D:E3:CE:F7',
+    // pincode: '031-45-555',
+    // PRODUCTION
+    name: 'Gardener',
+    username: 'CC:22:3D:E3:CE:F6',
+    pincode: '031-45-154'
 };
