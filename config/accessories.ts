7a601f21f23b489ed5511c156641c83024bcab8d
Alexandre Clément <web@alexandreclement.com>
2018-01-27 23:58:06 -0500

    Update accessories config

--- config/accessories.ts
+++ config/accessories.ts
@@ -3,46 +3,31 @@ import { Light } from '../accessories/light.accessory';
 import { TemperatureSensor } from '../accessories/temperature-sensor.accessory';
 import { DHTSensorDevice } from "../gpio/dht-sensor";
 import { Greenhouse } from '../accessories/greenhouse.accessory';
-import { GreenhouseServo } from '../accessories/greenhouse-servo.accessory';
 
-// const globalDHTSensorDevice = new DHTSensorDevice(4, {
-//   readingInterval: 30,
-//   calibration: {
-//     humidity: 0,
-//     temperature: +2,
-//   }
-// });
-
-const greenhouseRightDHTSensorDevice = new DHTSensorDevice(4, {
+const globalDHTSensorDevice = new DHTSensorDevice(4, {
   readingInterval: 30,
   calibration: {
-    humidity: 0,
+    humidity: -3,
     temperature: +2,
   }
 });
 
-const greenhouseCenterDHTSensorDevice = new DHTSensorDevice(14, {
+const greenhouseDHTSensorDevice = new DHTSensorDevice(23, {
   readingInterval: 30,
   calibration: {
-    humidity: 0,
+    humidity: +20,
     temperature: +2,
   }
 });
 
 export default {
-  //'humidity': new HumiditySensor('Humidité globale', globalDHTSensorDevice),
+  'humidity': new HumiditySensor('Humidité globale', globalDHTSensorDevice),
   'light-top': new Light('Lumière haut', 11),
   'light-bottom': new Light('Lumière bas', 13),
-  //'temperature': new TemperatureSensor('Température', globalDHTSensorDevice),
-  'greenhouse-right': new Greenhouse('Miniserre - Droite', {
-    'humidity': new HumiditySensor('MS. D. - Humidité', greenhouseRightDHTSensorDevice),
-    'temperature': new TemperatureSensor('MS. D. - Température', greenhouseRightDHTSensorDevice),
-    //'servo': new GreenhouseServo('Miniserre - Servo', null),
-  }),
-  'greenhouse-center': new Greenhouse('Miniserre - Centre', {
-    'humidity': new HumiditySensor('MS. C. - Humidité', greenhouseCenterDHTSensorDevice),
-    'temperature': new TemperatureSensor('MS. C. - Température', greenhouseCenterDHTSensorDevice),
-    //'servo': new GreenhouseServo('Miniserre - Servo', null),
+  'temperature': new TemperatureSensor('Température', globalDHTSensorDevice),
+  'greenhouse': new Greenhouse('Miniserre', {
+    'humidity': new HumiditySensor('Miniserre - Humidité', greenhouseDHTSensorDevice),
+    'temperature': new TemperatureSensor('Miniserre - Température', greenhouseDHTSensorDevice),
   }),
   //'moisture-1': new MoistureSensor('Hum. plante 1'),
 };
--- dist/config/accessories.js
+++ dist/config/accessories.js
@@ -5,38 +5,27 @@ var light_accessory_1 = require("../accessories/light.accessory");
 var temperature_sensor_accessory_1 = require("../accessories/temperature-sensor.accessory");
 var dht_sensor_1 = require("../gpio/dht-sensor");
 var greenhouse_accessory_1 = require("../accessories/greenhouse.accessory");
-// const globalDHTSensorDevice = new DHTSensorDevice(4, {
-//   readingInterval: 30,
-//   calibration: {
-//     humidity: 0,
-//     temperature: +2,
-//   }
-// });
-var greenhouseRightDHTSensorDevice = new dht_sensor_1.DHTSensorDevice(4, {
+var globalDHTSensorDevice = new dht_sensor_1.DHTSensorDevice(4, {
     readingInterval: 30,
     calibration: {
-        humidity: 0,
+        humidity: -3,
         temperature: +2
     }
 });
-var greenhouseCenterDHTSensorDevice = new dht_sensor_1.DHTSensorDevice(14, {
+var greenhouseDHTSensorDevice = new dht_sensor_1.DHTSensorDevice(23, {
     readingInterval: 30,
     calibration: {
-        humidity: 0,
+        humidity: +20,
         temperature: +2
     }
 });
 exports["default"] = {
-    //'humidity': new HumiditySensor('Humidité globale', globalDHTSensorDevice),
+    'humidity': new humidity_sensor_accessory_1.HumiditySensor('Humidité globale', globalDHTSensorDevice),
     'light-top': new light_accessory_1.Light('Lumière haut', 11),
     'light-bottom': new light_accessory_1.Light('Lumière bas', 13),
-    //'temperature': new TemperatureSensor('Température', globalDHTSensorDevice),
-    'greenhouse-right': new greenhouse_accessory_1.Greenhouse('Miniserre - Droite', {
-        'humidity': new humidity_sensor_accessory_1.HumiditySensor('MS. D. - Humidité', greenhouseRightDHTSensorDevice),
-        'temperature': new temperature_sensor_accessory_1.TemperatureSensor('MS. D. - Température', greenhouseRightDHTSensorDevice)
-    }),
-    'greenhouse-center': new greenhouse_accessory_1.Greenhouse('Miniserre - Centre', {
-        'humidity': new humidity_sensor_accessory_1.HumiditySensor('MS. C. - Humidité', greenhouseCenterDHTSensorDevice),
-        'temperature': new temperature_sensor_accessory_1.TemperatureSensor('MS. C. - Température', greenhouseCenterDHTSensorDevice)
+    'temperature': new temperature_sensor_accessory_1.TemperatureSensor('Température', globalDHTSensorDevice),
+    'greenhouse': new greenhouse_accessory_1.Greenhouse('Miniserre', {
+        'humidity': new humidity_sensor_accessory_1.HumiditySensor('Miniserre - Humidité', greenhouseDHTSensorDevice),
+        'temperature': new temperature_sensor_accessory_1.TemperatureSensor('Miniserre - Température', greenhouseDHTSensorDevice)
     })
 };
