99923d66c717341d708cbd2afa688dade5be46e7
Alexandre Clément <web@alexandreclement.com>
2018-01-28 12:39:28 -0500

    Simplify process exiting

--- dist/gardener.js
+++ dist/gardener.js
@@ -3,24 +3,11 @@ exports.__esModule = true;
 var accessory_manager_1 = require("./accessory-manager");
 var garden_monitor_1 = require("./garden-monitor");
 // Register listeners for process shutdown
-process.on('uncaughtException', function (err) {
-    console.trace(err);
-    setTimeout(function () { process.exit(1); }, 1000);
-    garden_monitor_1.GardenMonitor.announce(garden_monitor_1.LOG_TYPE.SHUTDOWN, ' ❗️  Gardener shutdown with error');
-    accessoryManager.shutdownAll(function () {
-        process.exit(1);
-    });
-});
 process.on('exit', function () {
     garden_monitor_1.GardenMonitor.closeDatabase();
 });
 process.on('SIGINT', function () {
-    garden_monitor_1.GardenMonitor.announce(garden_monitor_1.LOG_TYPE.STOP, ' 🛑  Gardener stopped', function () {
-        garden_monitor_1.GardenMonitor.closeDatabase();
-        accessoryManager.shutdownAll(function () {
-            process.exit(0);
-        });
-    });
+    garden_monitor_1.GardenMonitor.closeDatabase();
 });
 var lights_controller_1 = require("./controllers/lights.controller");
 // Init accessory manager
