feaadb70422b7358c70f8937353370df7caa0df2
Alexandre Clément <web@alexandreclement.com>
2018-05-19 22:54:55 -0400

    Revert "Update lights config"
    This reverts commit b652505756ef859a37b4fbe829f14fc48f35d566.

--- config/lights.ts
+++ config/lights.ts
@@ -1,6 +1,6 @@
 export default {
   // Time of day when lights are turned ON (HH:mm)
-  lightingUpTime: '06:00',
+  lightingUpTime: '07:00',
   // Time of day when lights are turned OFF (HH:mm)
-  lightingDownTime: '23:00',
+  lightingDownTime: '22:00',
 }
--- dist/config/lights.js
+++ dist/config/lights.js
@@ -2,7 +2,7 @@
 exports.__esModule = true;
 exports["default"] = {
     // Time of day when lights are turned ON (HH:mm)
-    lightingUpTime: '06:00',
+    lightingUpTime: '07:00',
     // Time of day when lights are turned OFF (HH:mm)
-    lightingDownTime: '23:00'
+    lightingDownTime: '22:00'
 };
